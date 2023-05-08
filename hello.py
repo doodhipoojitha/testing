7dda898067b218099810381408345ca2806c98f8
doodhipoojitha <doodhipoojitha@gmail.com>
2023-05-08 15:53:06 +0530

    while loop

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("hello iam added to github repo")
+while True:
+    user_input = int(input("pls enter the values for iteration:"))
+    if user_input>0:
+        for i in range(user_input):
+            print(i)
+        continue
+    else:
+        print("exit")
+        break
